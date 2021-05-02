3a5ec83a69b75cc37221253a972faa418da6305b
Evan Pratten <ewpratten@gmail.com>
2021-05-02 13:07:21 -0400

    db

--- apps/cloudlog/setup.sql
+++ apps/cloudlog/setup.sql
@@ -1,6 +1,7 @@
 -- create the databases
 CREATE DATABASE IF NOT EXISTS cloudlog;
 CREATE DATABASE IF NOT EXISTS options;
+CREATE DATABASE IF NOT EXISTS cloudlog.options;
 
 -- create the users for each database
 CREATE USER 'admin' IDENTIFIED BY 'adminpass';
